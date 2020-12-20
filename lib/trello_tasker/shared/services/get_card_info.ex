8cba9032c8a0318de4de09a9d0426694729ef5b2
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-12-19 22:25:21 -0300

    add service get card info.

---
+++ lib/trello_tasker/shared/services/get_card_info.ex
@@ -0,0 +1,10 @@
+defmodule TrelloTasker.Shared.Services.GetCardInfo do
+  alias TrelloTasker.Cards
+  alias TrelloTasker.Shared.Services.Trello
+  alias TrelloTasker.Shared.Providers.CacheProvider.CardCacheClient
+
+  @table "cards-list"
+
+  def execute() do
+  end
+end
